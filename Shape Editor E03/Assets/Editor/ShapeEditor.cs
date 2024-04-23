ced9af6489d3858c67984a02793d324a310bbac3
haftuiop <p3t3rbruc3@gmail.com>
2024-04-23 23:01:29 +0100

    savepoint3

--- Shape Editor E03/Assets/Editor/ShapeEditor.cs
+++ Shape Editor E03/Assets/Editor/ShapeEditor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿#if UNITY_EDITOR
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -373,3 +374,5 @@ public class ShapeEditor : Editor
     }
 
 }
+
+#endif
